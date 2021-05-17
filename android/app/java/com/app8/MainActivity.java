65992f8900878feba79bbea33423e138b8f6616a
maryummm <80543776+maryummm@users.noreply.github.com>
2021-05-17 12:26:07 +0530

    Add files via upload

---
+++ android/app/java/com/app8/MainActivity.java
@@ -0,0 +1,13 @@
+package com.app8;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  @Override
+  protected String getMainComponentName() {
+    return "app8";
+  }
+
+}
+
